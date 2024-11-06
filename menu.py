49d29f987785d0e58cae681fbc23b450b64f53d8
AlanBogarin <bogarin01alan@gmail.com>
2024-11-06 11:46:10 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ from empleados import gestion_empleados
 from asistencias import gestion_asistencia
 from nominas import calculo_nomina
 from comprobantes import generacion_comprobantes
+from utilidades import esperar_tecla, limpiar_pantalla, pedir_numero
 
 def menu() -> None:
     """Opciones del menu principal
@@ -12,3 +13,21 @@ def menu() -> None:
     - Generación de comprobantes
     - Salir
     """
+    while True:
+        limpiar_pantalla()
+        # 50 = 17 + 1 + 14 + 1 + 17
+        print("=" * 17, "MENU PRINCIPAL", "=" * 17)
+        print("1. Gestión de empleados")
+        print("2. Gestión de asistencias")
+        print("3. Cálculo de Nómina")
+        print("4. Generación de comprobantes")
+        print("0. Salir")
+        print("=" * 50)
+        match pedir_numero("Ingrese una opcion: "):
+            case 1: gestion_empleados()
+            case 2: gestion_asistencia()
+            case 3: calculo_nomina()
+            case 4: generacion_comprobantes()
+            case 0: break
+            case _: print("Opcion Invalida")
+        esperar_tecla()
