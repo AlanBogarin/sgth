97263c1a2e48da124d73dce8009256d555427338
AlanBogarin <bogarin01alan@gmail.com>
2024-11-08 18:36:12 -0300

    Add files via upload

--- asistencias.py
+++ asistencias.py
@@ -1,3 +1,9 @@
+from utilidades import esperar_tecla, limpiar_pantalla, pedir_numero
+
+def ausencias_llegadas_tardias(): ...
+def vacaciones_permisos(): ...
+def incapacidades_licencias(): ...
+
 def gestion_asistencia() -> None:
     """Opciones del menu
 
@@ -6,3 +12,18 @@ def gestion_asistencia() -> None:
     - Gestión de incapacidades y licencias
     - Regresar al menu principal
     """
+    while True:
+        limpiar_pantalla()
+        print("=" * 15, "GESTION ASISTENCIA", "=" * 15)
+        print("1. Registro de ausencias y llegadas tardias")
+        print("2. Registro de vacaciones, permisos y horas extra")
+        print("3. Gestión de incapacidades y licencias")
+        print("0. Regresar al menu principal")
+        print("=" * 50)
+        match pedir_numero("Ingrese una opcion: "):
+            case 1: ausencias_llegadas_tardias()
+            case 2: vacaciones_permisos()
+            case 3: incapacidades_licencias()
+            case 0: break
+            case _: print("Opción Invalida")
+        esperar_tecla()
--- empleados.py
+++ empleados.py
@@ -1,3 +1,15 @@
+from modelos import Empleado
+
+empleados: list[Empleado] = []
+"""Lista de todos los empleados"""
+
+def buscar_empleado() -> Empleado | None:
+    """Busca un empleado en la lista de empleados
+
+    Si encuentra una conincidencia, retorna la instancia,
+    sino, devuelve None
+    """
+
 def gestion_empleados() -> None:
     """Opciones del menu
 
