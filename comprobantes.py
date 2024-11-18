364a709ba34b20abbe8bb6953bb5f6d479eef588
AlanBogarin <bogarin01alan@gmail.com>
2024-11-18 19:34:49 -0300

    .

--- comprobantes.py
+++ comprobantes.py
@@ -2,5 +2,7 @@ def generacion_comprobantes() -> None:
     """Descripcion del PDF
 
     - Emisión de recibos de pago detallados para los empleados. 
-    - Generación de informes y reportes para Talento humano (Monto total del sueldo a ser pagado, Monto total por IPS empleado/empresa, Detalle de descuentos y pagos en un rango de meses para un empleado, etc.)
+    - Generación de informes y reportes para Talento humano (Monto total
+        del sueldo a ser pagado, Monto total por IPS empleado/empresa, Detalle
+        de descuentos y pagos en un rango de meses para un empleado, etc.)
     """
